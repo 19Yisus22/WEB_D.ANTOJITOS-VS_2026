e8a1df26ed2c9b97c67def2f560b123871195305
YisusPrime <jesusgabrielrodriguezrubio0@gmail.com>
2026-06-08 00:24:37 -0500

    D'Antojitos©

--- static/js/general_js/login_registro.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (typeof loadGoogleButton === 'function')  loadGoogleButton();
-    if (typeof initLoginForm === 'function')     initLoginForm();
-    if (typeof initRegistroForm === 'function')  initRegistroForm();
-    if (typeof initStep2 === 'function')         initStep2();
-});
